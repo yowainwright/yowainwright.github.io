e5396776e968d70bb0ee8a9a4f9b3aefa004e2a4
Jeffry Wainwright <jeffry_j_wainwright@yahoo.com>
2014-05-25 00:58:05 -0700

    Minor Updates
    Additions to the README file mainly :}

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 yowainwright.github.io
 ======================
+
+This is the personal webpage of Jeffry [Jeff] Wainwright. 
+It's built using Jekyll with Semantic UI. It also uses 
+slide js, waypoints js and hopefully more will be baked in
+as it grows.
--- scripts/jeffry.in.scripts.js
+++ scripts/jeffry.in.scripts.js
@@ -76,5 +76,22 @@ $( document ).ready(function() {
   if ($('div').hasClass('peek')) {
     waypointsRun();
   }
-  
+   
+  //lets load some stuff
+  /*$( window ).scroll(function() {
+    $('#leftButton').fadeOut('slow');
+  });
+  $.fn.scrollStopped = function(callback) {           
+        $(this).scroll(function(){
+            var self = this, $this = $(self);
+            if ($this.data('scrollTimeout')) {
+              clearTimeout($this.data('scrollTimeout'));
+            }
+            $this.data('scrollTimeout', setTimeout(callback,250,self));
+        });
+    };
+$(window).scrollStopped(function(){
+    $('#leftButton').fadeIn('fast');
+});*/
+
 });
