5906272c1a04540cd13955cb0d6e962e26ace603
Jeff Wainwright <jjwainwright2@gmail.com>
2016-09-17 15:47:03 -0700

    updates styles

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('copy', function() {
-  gulp.src('bower_components/scss-utility-mixins/src/mixins/**/*.scss')
-    .pipe(gulp.dest('_sass/components'));
-});
