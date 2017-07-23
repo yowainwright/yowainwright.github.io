58e993bc360045387c9b316078686dbd3ced0e08
Jeff Wainwright <jjwainwright2@gmail.com>
2017-07-23 13:15:33 -0700

    [update] gtm tag added

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `GTM-NRFFQ9`,
       },
     },
     `gatsby-plugin-offline`,
