50c3a59b75fd65633b3f9322116d840188b7eb24
Jeff Wainwright <yowainwright@gmail.com>
2017-08-28 16:33:59 -0700

    Updates

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('copy', function() {
-	gulp.src('node_modules/sassimple/mixins/**/*.scss')
- 		.pipe(gulp.dest('_sass/sassimple/'));
-}); 
-
-gulp.task('default', ['copy']);
