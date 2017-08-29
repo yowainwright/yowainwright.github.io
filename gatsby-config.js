2a78b879be77a7aed5c9946fe39f14bc9eef5cd3
Jeff Wainwright <yowainwright@gmail.com>
2017-08-28 19:45:40 -0700

    [bug] copies images over

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,59 @@ module.exports = {
         trackingId: 'GTM-NRFFQ9',
       },
     },
+    {
+    resolve: `gatsby-plugin-manifest`,
+      options: {
+        "name": "Jeffry.in",
+        "short_name": "Jeffry.in",
+        "start_url": "https://jeffry.in",
+        "display": "standalone",
+        "theme_color": "#ffffcc",
+        "background_color": "#FFFFFF",
+        "icons": [
+          {
+          "src": "https://yowainwright.imgix.net/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+          "src": "https://yowainwright.imgix.net/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+          "src": "https://yowainwright.imgix.net/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png"
+          },
+          {
+          "src": "https://yowainwright.imgix.net/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+          "src": "https://yowainwright.imgix.net/icon-152x152.png",
+            "sizes": "152x152",
+            "type": "image/png"
+          },
+          {
+          "src": "https://yowainwright.imgix.net/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+          "src": "https://yowainwright.imgix.net/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+          "src": "https://yowainwright.imgix.net/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+      },
+    },
     'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
