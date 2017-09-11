895b3904ef03034432e8a255a47392673d5e6eaf
Jeff Wainwright <yowainwright@gmail.com>
2017-09-10 17:49:36 -0700

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-3458da6a659c8151bd53.js","6be9da51783f21869d5ec32d866b9457"],["/app-86c56191d4022c96bbe8.js","83d9c1b8389d8c8782054ea941aac27a"],["/commons-3c8704a40009f722bd68.js","8ecbe3ee17dd9052c6cdcf458cca03ce"],["/index.html","d2fa5641e9262c76a1b7d8e1c14de036"],["/manifest.json","4914d7669e829af0507892c32a814568"],["/offline-plugin-app-shell-fallback/index.html","68383a8942a32d03bcd0d83c6a359061"]];
+var precacheConfig = [["/app-3458da6a659c8151bd53.js","6be9da51783f21869d5ec32d866b9457"],["/app-86c56191d4022c96bbe8.js","83d9c1b8389d8c8782054ea941aac27a"],["/app-bcb262694398689460f1.js","29e60555c0431bbf1f443d5c1ae64714"],["/commons-3c8704a40009f722bd68.js","8ecbe3ee17dd9052c6cdcf458cca03ce"],["/index.html","d6c08c25b04e2e9cb232ca4f651159f3"],["/manifest.json","4914d7669e829af0507892c32a814568"],["/offline-plugin-app-shell-fallback/index.html","4893a3df48a6f5e1aad597b212384b6a"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
