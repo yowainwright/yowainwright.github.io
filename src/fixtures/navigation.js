72f55b3700bef17bba943f4358ab51a3f91d09b8
Jeff Wainwright <yowainwright@gmail.com>
2018-10-14 12:56:17 -0700

    [init] adds initial fixtures

---
+++ src/fixtures/navigation.js
@@ -0,0 +1,17 @@
+export const navItems = [
+  {
+    alias: 'home',
+    name: 'Jeffry.in',
+    path: '/',
+  },
+  {
+    alias: 'archive',
+    name: 'Archive',
+    path: '/archive/',
+  },
+  {
+    alias: 'about',
+    name: 'About',
+    path: '/about/',
+  },
+]
---
+++ src/fixtures/social.js
@@ -0,0 +1,75 @@
+export const socialItems = [
+  {
+    alias: 'github',
+    name: 'Github',
+    path: 'https://github.com/yowainwright',
+    level: 'main',
+  },
+  {
+    alias: 'twitter',
+    name: 'Twitter',
+    path: 'https://twitter.com/yowainwright',
+    level: 'main',
+  },
+  {
+    alias: 'npm',
+    name: 'NPMJS',
+    path: 'https://www.npmjs.com/~yowainwright',
+    level: 'main',
+  },
+  {
+    alias: 'instagram',
+    name: 'Instragram',
+    path: 'https://instagram.com/yowainwright',
+    level: 'social',
+  },
+  {
+    alias: 'medium',
+    name: 'Medium',
+    path: 'https://medium.com/@yowainwright',
+    level: 'social',
+  },
+  {
+    alias: 'tumblr',
+    name: 'Tumblr',
+    path: 'http://ratherbe.in',
+    level: 'social',
+  },
+  {
+    alias: 'strava',
+    name: 'Strava',
+    path: 'https://www.strava.com/athletes/722335',
+    level: 'social',
+  },
+  {
+    alias: 'linkedin',
+    name: 'LinkedIn',
+    path: 'https://www.linkedin.com/in/jeffrywainwright/',
+    level: 'social',
+  },
+  {
+    alias: 'spotify',
+    name: 'Spotify',
+    path: 'https://open.spotify.com/user/127322575?si=6zrJlJX9RcaOk5mp-iJnJg',
+    level: 'user',
+  },
+  {
+    alias: 'vimeo',
+    name: 'Vimeo',
+    path: 'https://vimeo.com/yowainwright',
+    level: 'user',
+  },
+  {
+    alias: 'youtube',
+    name: 'YouTube',
+    path: 'https://www.youtube.com/user/yowainwright',
+    leve;: 'user',
+  },
+   {
+     alias: 'vsco',
+     name: 'VSCO',
+     path: 'https://vsco.co/yowainwright/images/1',
+     leve;: 'user',
+   },
+
+]
