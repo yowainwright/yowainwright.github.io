854875f8da1bac79c52c6cd5c65ccb479dbece8f
Jeff Wainwright <yowainwright@gmail.com>
2019-08-22 12:05:40 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-89021404ce8ec56a4fe2.js"
+    "url": "webpack-runtime-a56c13316aef98155706.js"
   },
   {
     "url": "app-09707b34dc8d8c9da0e7.js"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "94a5a8c5160a3631c0298a7a85295b08"
+    "revision": "9ddb7ee2ea416f5e14fc483f22c54775"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5442298582f88f8b295fb37f2385837b"
+    "revision": "d79ff18c5b59bbbbc8730cd4200dc11e"
   },
   {
     "url": "0.2f2933dc343b474e635f.css"
@@ -49,58 +49,10 @@ self.__precacheManifest = [
     "url": "component---src-pages-index-jsx.c658796057851eca7013.css"
   },
   {
-    "url": "google-fonts/s/notoserif/v6/ga6Iaw1J5X9T9RW6j9bNfFcWaA.woff2",
-    "revision": "f7fb9922ab0013f0ee2f11ca54ed6b88"
+    "url": "component---src-pages-index-jsx-ba849d3cd7fc6d846ad1.js"
   },
   {
-    "url": "google-fonts/s/notoseriftc/v1/XLY9IZb5bJNDGYxLBibeHZ0BvvMpXX5MTw.woff2",
-    "revision": "c1cece1a4145a234bf5d95fb3a2349fc"
-  },
-  {
-    "url": "google-fonts/s/notoseriftc/v1/XLYgIZb5bJNDGYxLBibeHZ0BtnAOSA.woff2",
-    "revision": "f7296b651f59c9470cbc98fe8a796a74"
-  },
-  {
-    "url": "google-fonts/s/oswald/v16/TK3hWkUHHAIjg75-6hwTus9C.woff2",
-    "revision": "541a863959122f29c9961095cdcbb5c2"
-  },
-  {
-    "url": "google-fonts/s/oswald/v16/TK3iWkUHHAIjg752GT8G.woff2",
-    "revision": "f15aa285863274b4f6ed578caa76565e"
-  },
-  {
-    "url": "google-fonts/s/oswald/v17/TK3hWkUHHAIjg75-6hwTus9C.woff2",
-    "revision": "dc4d5749ac2b883f507ceca023a21ccc"
-  },
-  {
-    "url": "google-fonts/s/playfairdisplay/v13/nuFiD-vYSZviVYUb_rj3ij__anPXDTzYgA.woff2",
-    "revision": "203179d16cd511feb9d8691f27926c3b"
-  },
-  {
-    "url": "google-fonts/s/playfairdisplay/v13/nuFlD-vYSZviVYUb_rj3ij__anPXBb__lW4e5g.woff2",
-    "revision": "4679353199d23f37e5aceb461a374027"
-  },
-  {
-    "url": "google-fonts/s/playfairdisplay/v14/nuFiD-vYSZviVYUb_rj3ij__anPXDTzYgA.woff2",
-    "revision": "496b0ee7c32c16da8c6289fa8aae5268"
-  },
-  {
-    "url": "google-fonts/s/playfairdisplay/v14/nuFlD-vYSZviVYUb_rj3ij__anPXBb__lW4e5g.woff2",
-    "revision": "18cc8f34bd972a20d01df12408aeb0a0"
-  },
-  {
-    "url": "google-fonts/s/playfairdisplay/v15/nuFiD-vYSZviVYUb_rj3ij__anPXDTzYgA.woff2",
-    "revision": "496b0ee7c32c16da8c6289fa8aae5268"
-  },
-  {
-    "url": "google-fonts/s/playfairdisplay/v15/nuFlD-vYSZviVYUb_rj3ij__anPXBb__lW4e5g.woff2",
-    "revision": "18cc8f34bd972a20d01df12408aeb0a0"
-  },
-  {
-    "url": "component---src-pages-index-jsx-06b79c087964f825557e.js"
-  },
-  {
-    "url": "0-6c2ed1d2da2c26bc9b00.js"
+    "url": "0-c86c586374a67dc42ee0.js"
   },
   {
     "url": "static/d/552/path---index-6a9-qx1uACEUubxacdStnzfWvsLHS4.json",
