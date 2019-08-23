042ca236a6d340f0a2d4222373f81013b64cf133
Jeff Wainwright <yowainwright@gmail.com>
2019-08-23 13:08:04 -0700

    updates pages/index.js (#279)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,75 +4,60 @@ import Link from 'gatsby-link'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
+
+export const PostHeader = ({ title, path, date }) => (
+  <header className='post__header'>
+    <h2 className='post__title'><Link to={path}>{title}</Link></h2>
+    <time>{date}</time>
+  </header>
+)
+
+export const PostFigure = ({ image, path }) => image ? (
+    <figure itemType='http://schema.org/ImageObject'>
+      <Link to={path}>
+        <img src={`${image}?w=800&&fit=crop&crop=focalpoint&auto=format`} itemProp='contentURL' alt='Post Figure' />
+      </Link>
+    </figure>
+  ) : null
+
+export const Posts = ({ posts }) => posts.map((post, i) => {
+  if (i > 11) return
+
+  const pNode = post.node
+  const path = get(post, 'node.frontmatter.path') || pNode.path
+  if (path === '/404/' || path === '/about' || path === '/about/') return
+
+  const title = get(post, 'node.frontmatter.title') || pNode.title
+  const date = get(post, 'node.frontmatter.date') || pNode.date
+  const description = get(post, 'node.frontmatter.meta') || pNode.meta
+  const image = get(post, 'node.frontmatter.featured_image') || post.node.featured_image
+
+  return (
+    <article key={i} className='post--article'>
+      <PostHeader title={title} path={path} date={date} />
+      <PostFigure image={image} path={path} />
+      <p>{description} <Link className='post__link--read-more' to={path}>[...]</Link></p>
+      <hr />
+    </article>
+  )
+})
+
 export default class BlogIndex extends Component {
-  constructor (props) {
-    super(props)
-    this.title = 'Jeffry.in'
-    this.description = 'Jeffry.in is the blog of Jeffry Wainwright, an engineer living in California.'
-    this.posts = get(this, 'props.data.allMarkdownRemark.edges')
-  }
-
-  generatePostHeader = (title, path, date) => {
-    return (
-      <header className='post__header'>
-        <h2 className='post__title'><Link to={path}>{title}</Link></h2>
-        <time>{date}</time>
-      </header>
-    )
-  }
-
-  generatePostFigure = (image, path) => {
-    if (typeof image === 'undefined') return ''
-    return (
-      <figure itemType='http://schema.org/ImageObject'>
-        <Link to={path}>
-          <img src={`${image}?w=800&&fit=crop&crop=focalpoint&auto=format`} itemProp='contentURL' alt='Post Figure' />
-        </Link>
-      </figure>
-    )
-  }
-
-  generatePosts = () => {
-    const postItems = []
-    this.posts.forEach((post, i) => {
-      if (i > 11) return
-
-      const pNode = post.node
-      const path = get(post, 'node.frontmatter.path') || pNode.path
-      if (path === '/404/' || path === '/about' || path === '/about/') return
-
-      const title = get(post, 'node.frontmatter.title') || pNode.title
-      const date = get(post, 'node.frontmatter.date') || pNode.date
-      const description = get(post, 'node.frontmatter.meta') || pNode.meta
-      const image = get(post, 'node.frontmatter.featured_image') || post.node.featured_image
-      const header = this.generatePostHeader(title, path, date)
-      const figure = this.generatePostFigure(image, path)
-
-      postItems.push(
-        <article key={i} className='post--article'>
-          {header}
-          {figure}
-          <p>{description} <Link className='post__link--read-more' to={path}>[...]</Link></p>
-          <hr />
-        </article>
-      )
-    })
-
-    return postItems
-  }
-
   render () {
+    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+    const title = 'Jeffry.in'
+    const description = 'Jeffry.in is the blog of Jeffry Wainwright, an engineer living in California.'
     return (
       <Layout>
         <main className='main'>
-          <Helmet title={this.title}>
-            <meta name='twitter:description' property='og:description' content={`${this.description}`} />
+          <Helmet title={title}>
+            <meta name='twitter:description' property='og:description' content={description} />
             <link rel='canonical' href='https://jeffry.in' itemProp='url' />
             <meta name='twitter:url' property='og:url' content='https://jeffry.in' />
-            <meta name='twitter:title' property='og:title' content={`${this.title}`} />
+            <meta name='twitter:title' property='og:title' content={title} />
           </Helmet>
           <hr />
-          {this.generatePosts()}
+          <Posts posts={posts} />
         </main>
       </Layout>
     )
