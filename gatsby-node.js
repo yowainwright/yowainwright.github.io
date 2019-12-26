a58ace20ce1dab1131a714a78e221ba306d3e9f4
Jeff Wainwright <yowainwright@gmail.com>
2019-12-25 22:58:09 -0800

    Init rss feed (#349)
    * feat(rss): adds initial rss feed
    
    * fix(font): updates font

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
@@ -33,4 +34,16 @@ exports.createPages = ({ graphql, actions }) => {
       })
     )
   })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
 }
