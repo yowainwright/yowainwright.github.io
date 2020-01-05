05b82ca836e60cb80d4ff774fa8a1857f8525396
Jeff Wainwright <yowainwright@gmail.com>
2020-01-04 23:36:07 -0800

    fix(share-list): adds social icons (#357)

--- src/components/post/basic.jsx
+++ src/components/post/basic.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { PostHeader } from './header'
 import { Link } from 'gatsby'
+import { ShareList } from '../ShareList'
 
 export const BasicPost = ({ date, description, path, title }) => (
   <article className='post--article'>
@@ -9,6 +10,7 @@ export const BasicPost = ({ date, description, path, title }) => (
       {description}{' '}
       <Link className='post__link--read-more' to={path}>[...]</Link>
     </p>
+    <ShareList name="blog-bottom" path={path} title={title} />
     <hr />
   </article>
 )
