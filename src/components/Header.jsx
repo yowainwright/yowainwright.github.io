4b680feb2256bca568b8e381f334eb4de86f1335
Jeff Wainwright <yowainwright@gmail.com>
2020-02-03 13:26:19 -0800

    fix(header): adds initial work for the hamburger (#383)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,59 +1,62 @@
-import React, { Component } from 'react'
+import React, { FunctionComponent } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-export default class Header extends Component {
-  static defaultProps = {
-    componentName: 'site-nav',
-    navItems: [
-      {
-        alias: 'about',
-        name: 'About',
-        path: '/about/',
-      },
-      {
-        alias: 'archive',
-        inApp: true,
-        name: 'Archive',
-        path: '/archive/',
-      },
-    ],
-  }
 
-  render () {
-    const { componentName, navItems } = this.props
-    return (
-      <nav id={componentName} className={componentName} role='navigation' itemType='http://schema.org/SiteNavigationElement'>
-        <div className={`${componentName}__container`}>
-          <Link to="/" className={`${componentName}__main-item-link`}>
-            <div className={`${componentName}__main-item`}>
-              <h3 className={`${componentName}__main-item-symbol`}>j</h3>
-              <h2 className={`${componentName}__main-item-title`}>Jeffry.in</h2>
-            </div>
-          </Link>
-          <ol className={`${componentName}__items`}>
-            {navItems.map(({ alias, name, path }, i) => {
-              return (
-                <li
-                  key={i}
-                  className={`${componentName}__item ${componentName}__item--${alias}`}
-                >
-                  <Link
-                    className={`${componentName}__link ${componentName}__link--${alias}`}
-                    to={path}
-                  >
-                    {name}
-                  </Link>
-                </li>
-              )
-            })}
-          </ol>
-        </div>
-      </nav>
-    )
-  }
+
+const navData = {
+  componentName: 'site-nav',
+  navItems: [
+    {
+      alias: 'about',
+      name: 'About',
+      path: '/about/',
+    },
+    {
+      alias: 'archive',
+      inApp: true,
+      name: 'Archive',
+      path: '/archive/',
+    },
+  ],
 }
 
-Header.propTypes = {
-  componentName: PropTypes.string.isRequired,
-  navItems: PropTypes.array.isRequired,
+export const Hamburger = () => (<button className="nav-button nav-button--hamburger">☰</button>)
+
+export const NavList = ({ componentName, navItems }) => (
+<ol className={`${componentName}__items`}>
+{navItems.map(({ alias, name, path }, i) => {
+  return (
+    <li
+      key={i}
+      className={`${componentName}__item ${componentName}__item--${alias}`}
+    >
+      <Link
+        className={`${componentName}__link ${componentName}__link--${alias}`}
+        to={path}
+      >
+        {name}
+      </Link>
+    </li>
+  )
+})}
+</ol>
+)
+
+const Header = () => {
+  const { componentName, navItems } = navData
+  return (
+    <nav id={componentName} className={componentName} role='navigation' itemType='http://schema.org/SiteNavigationElement'>
+      <div className={`${componentName}__container`}>
+        <Link to="/" className={`${componentName}__main-item-link`}>
+          <div className={`${componentName}__main-item`}>
+            <h3 className={`${componentName}__main-item-symbol`}>j</h3>
+            <h2 className={`${componentName}__main-item-title`}>Jeffry.in</h2>
+          </div>
+        </Link>
+        <NavList componentName={componentName} navItems={navItems} />
+      </div>
+    </nav>
+  )
 }
+
+export default Header
