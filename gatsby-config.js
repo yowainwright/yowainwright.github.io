0380acbadd0d83feba0d2274490a41714fc9bf79
yowainwright <yowainwright@gmail.com>
2020-04-05 16:46:56 -0700

    fix: updates config; updates images

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,10 +20,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-tinacms',
       options: {
-        plugins: [
-          'gatsby-tinacms-git',
-          'gatsby-tinacms-remark',
-        ],
+        plugins: ['gatsby-tinacms-git', 'gatsby-tinacms-remark'],
         sidebar: {
           hidden: NODE_ENV === 'production',
         },
@@ -45,58 +42,57 @@ module.exports = {
         ],
       },
     },
-    'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        'name': 'Jeffry.in',
-        'short_name': 'Jeffry.in',
-        'start_url': 'https://jeffry.in',
-        'display': 'standalone',
-        'theme_color': '#ffffcc',
-        'background_color': '#FFFFFF',
-        'icons': [
+        name: 'Jeffry.in',
+        short_name: 'Jeffry.in',
+        start_url: 'https://jeffry.in',
+        display: 'standalone',
+        theme_color: '#ffffcc',
+        background_color: '#FFFFFF',
+        icons: [
           {
-            'src': '/assets/icon-72x72.png',
-            'sizes': '72x72',
-            'type': 'image/png'
+            src: '/assets/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
           },
           {
-            'src': '/assets/icon-96x96.png',
-            'sizes': '96x96',
-            'type': 'image/png'
+            src: '/assets/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
           },
           {
-            'src': '/assets/icon-128x128.png',
-            'sizes': '128x128',
-            'type': 'image/png'
+            src: '/assets/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
           },
           {
-            'src': '/assets/icon-144x144.png',
-            'sizes': '144x144',
-            'type': 'image/png'
+            src: '/assets/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
           },
           {
-            'src': '/assets/icon-152x152.png',
-            'sizes': '152x152',
-            'type': 'image/png'
+            src: '/assets/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
           },
           {
-            'src': '/assets/icon-192x192.png',
-            'sizes': '192x192',
-            'type': 'image/png'
+            src: '/assets/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            'src': '/assets/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
+            src: '/assets/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
           },
           {
-            'src': '/assets/icon-512x512.png',
-            'sizes': '512x512',
-            'type': 'image/png'
-          }
+            src: '/assets/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
         ],
       },
     },
