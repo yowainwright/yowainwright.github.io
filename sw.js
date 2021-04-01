11e30d73a3ed2636f037a398ffdba9782c281f1d
yowainwright <yowainwright@gmail.com>
2021-04-01 16:22:06 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-378887fbca140b8c10b9.js"
+    "url": "webpack-runtime-03236e635100320001ce.js"
   },
   {
     "url": "framework-61416065c0fba8268899.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c3920c8526209edd6e51bfde4add3ea9"
+    "revision": "0474b5a099942d539836ed94c2970b43"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
