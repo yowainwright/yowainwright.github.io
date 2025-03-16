922b1d08be0178fd11fcc74aac0758127984790c
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2025-03-15 23:30:12 -0700

    chore: udpates next build

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 
-module.exports = {
+export const sitemapConfig = {
   siteUrl: 'https://jeffry.in',
   generateRobotsTxt: true,
 }
+
+export default sitemapConfig;
