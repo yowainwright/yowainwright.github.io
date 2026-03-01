9b0caf16dfffb3ddec11181a40b16271780e2491
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2026-03-01 12:01:37 -0800

    fix: updates ogmeta component

--- lib/components/OgMeta/constants.ts
+++ lib/components/OgMeta/constants.ts
@@ -1,2 +1,6 @@
 export const SITE_URL = "https://jeffry.in";
 export const DEFAULT_AUTHOR = "Jeff Wainwright";
+export const DEFAULT_OG_IMAGE = "/og/default.png";
+export const OG_IMAGE_WIDTH = "1200";
+export const OG_IMAGE_HEIGHT = "630";
+export const TWITTER_HANDLE = "@yowainwright";
